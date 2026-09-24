a59cf43a8de2e0e46c17d75f915a1f6470ee5d42
Akash N <akashnigamforu@gmail.com>
2026-09-24 13:22:48 +0530

    First commit - Setup

---
+++ src/test/java/AkashSoftwareSolutions/SampleTest.java
@@ -0,0 +1,31 @@
+package AkashSoftwareSolutions;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @BeforeMethod 
+    public void login() {
+        System.out.println("Logged in successfully.");
+    }
+
+    @Test 
+    public void display1() {
+        System.out.println("Hello Seattle!");
+        Assert.assertTrue(true);
+    }
+
+    @Test 
+    public void display2() {
+        System.out.println("Hello Moscow!");
+        Assert.assertTrue(true);
+    }
+
+    @AfterMethod 
+    public void logout() {
+        System.out.println("Logged out successfully.");
+    }
+}
